f23a3cff3d39680b18fe7e760d8b01ae7f1aaa5b
Shubhankar singh <shubhankarsingh1122@gmail.com>
2025-11-15 19:48:51 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,4 +1,28 @@
-public class Employee {
+class Employee {
+
     
-    
+    int id;
+    double salary;
+    String name;   l
+
+   
+    Employee(int id, double salary, String name) {
+        this.id = id;
+        this.salary = salary;
+        this.name = name;
+    }
+    void display() {
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee Salary: " + salary);
+    }
+
+    public static void main(String[] args) {
+
+       
+        Employee emp1 = new Employee(101, 50000, "Rahul Sharma");
+
+       
+        emp1.display();
+    }
 }
