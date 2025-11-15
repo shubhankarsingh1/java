2febfa151fc38749915bd7c28e6080a18c9bb2b5
Shubhankar singh <shubhankarsingh1122@gmail.com>
2025-11-15 19:28:01 +0530

    Add files via upload
    write a program using bitwise operation.
    write a program using Break statement.

---
+++ Bitwise.java
@@ -0,0 +1,9 @@
+public class Bitwise {
+    public static void main(String[]args){
+        int a =5;
+        int result = (~a +1)*4;
+        System.out.println("Result"+ result);
+
+    }
+    
+}
